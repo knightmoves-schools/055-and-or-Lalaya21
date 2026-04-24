3c6195ce6091cdaec42e7f85360478714deadf3c
lalaya21 <lalaya.shuckahosee@students.msswarriors.org>
2026-04-24 18:32:33 +0000

    done1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function taxtables (state,taxExempt) {
+    if (state === "IA" && taxExempt === false) {
+        return "is taxable";
+    }
+}
+function isVehicle (hasWheels,canFLy,canSwim) {
+    if (hasWheels == true|| canSwim == true) { |
+        return "is vehicle";
+    }
+}
